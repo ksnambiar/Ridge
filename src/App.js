20b03d02fa41480c4827ba95c179bd8e6188a482
K Sidhartha Nambiar <nambiar.sidharth@gmail.com>
2019-02-05 16:25:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./Store";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "./Components/Navbar/Navigation";
+import Landing from "./Components/Common/Landing";
+import Footer from "./Components/Common/Footer";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navigation />
+            <Landing />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
