00e9bfd566711ebbd313c37f4caa3b26ae2ed3a7
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-04-28 06:45:59 +0530

    team page updated

--- src/Components/Common/Team.js
+++ src/Components/Common/Team.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import {Badge,Card,CardImg} from "react-bootstrap"
+import {Badge,Card,CardImg, Button} from "react-bootstrap"
 import Lakshya from "./TeamPics/Lakshya.jpg";
 import Pranath from "./TeamPics/Pranath.jpeg"
+import Abhishek2 from "./TeamPics/Abhishek2.jpeg"
 import Sidharth from "./TeamPics/Sidharth.jpeg"
+import Octicon, { getIconByName } from "@githubprimer/octicons-react";
+import { SocialIcon } from 'react-social-icons';
 function Team() {
   return (
     <div>
     <div>
-    <h3 class="w3-center">THE TEAM</h3>
+    <h3 class="w3-center">THE CORE TEAM</h3>
         <p class="w3-center w3-large">The ones who runs this site behind the scenes</p>
     </div>
     <div className="row">
    
-    <Card style={{width:"20rem"}} className=" ma2">
-    <CardImg variant="top" src={`${Sidharth}/100px180`} style={{height:"30rem"}}/>
+    <Card style={{width:"22rem"}} className=" ma2">
+    <CardImg variant="top" src={Sidharth} />
     <Card.Body>
     <h3>K Sidhartha Nambiar</h3>
     <h6>Popularly known as</h6><Badge variant="info">Nambiar</Badge>
     <p class="w3-opacity">Team Lead</p>
     <p>An ambivert coder who developes crush on every girl he sees</p>
-    <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>         
+    
+    <div className="row">
+    <div className="center">
+    <Button variant="dark" className="mh2">
+    <Octicon icon={getIconByName("mark-github")} />
+    </Button>
+
+    <Button variant="dark" className="mh2">
+    <Octicon icon={getIconByName("mail")} />
+    </Button>
+
+    <Button variant="dark" className="mh2">
+    <Octicon icon={getIconByName("device-mobile")} />
+    </Button>
+    
+    <SocialIcon url="https://www.facebook.com/sidharth.nambiar.35" />
+    </div>
+    </div>
     
     </Card.Body>
     </Card>
@@ -36,7 +56,7 @@ function Team() {
     </Card>
 
     <Card  style={{width:"22rem"}} className="ma2">
-    <CardImg variant="top"/>
+    <CardImg variant="top" src={Abhishek2}/>
     <Card.Body>
     <h3>Abhishek S</h3>
     <h6>Popularly known as</h6><Badge variant="info">Boss Ambi</Badge>
@@ -56,6 +76,13 @@ function Team() {
     <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>         
     </Card.Body>
     </Card>
+
+    </div>
+    <div className="row">
+    <div>
+    <h3 class="w3-center">THE CONTRIBUTORS</h3>
+        <p class="w3-center w3-large">The ones who helps us make this platform better every day</p>
+    </div>
 
     </div>
     </div>
--- src/api/Api_ref.js
+++ src/api/Api_ref.js
@@ -1,2 +1,2 @@
-export const local_host="https://blooming-gorge-84662.herokuapp.com";
-export const heroku_url="http://localhost:5000" 
+export const heroku_url="https://blooming-gorge-84662.herokuapp.com";
+export const local_host="http://localhost:5000" 
