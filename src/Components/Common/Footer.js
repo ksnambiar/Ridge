f2722221e8953a4bfcf143789446fd1e97219582
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-07-11 16:15:29 +0530

    some changes

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
 
-  return <div className="container-fluid">
+  return <div className="container-fluid" style={{position:'fixed',bottom:0}}>
   <div className="row  bg-dark pa2">
    <h6 className="center gray">© 2019 TecRidge inc.</h6>
   </div>
